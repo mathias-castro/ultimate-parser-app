70dc55597f83d81d0da8bf3171aea3b2f008d45d
A-Joacox <joacmercado@gmail.com>
2026-05-18 15:45:16 -0500

    FIX: Unused react var

--- frontend/src/components/results/HistoryPanel.tsx
+++ frontend/src/components/results/HistoryPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useHistory } from "../../hooks/useHistory";
 import type { HistoryEntry } from "../../types/parser";
 
