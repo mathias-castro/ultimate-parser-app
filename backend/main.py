3be76e56d93e24ceb132f2d624c8f2bb9ab2d6d5
Mathias Castro <mathias.castro@utec.edu.pe>
2026-05-17 22:49:35 -0500

    fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -4,14 +4,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
-from app.core.config import ALLOWED_ORIGINS, APP_NAME
+from app.core.config import APP_NAME
 
 app = FastAPI(title=APP_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
